9b361738b25e3b3e51ae3b558dcf7978d03ce428
aryan bramhane <aryanbramhane1@gmail.com>
2022-12-03 16:30:57 +0530

    adding basic website

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import Navbar from '../components/NavBar'
+import Footer from '../components/Footer'
+import Blogs from '../components/Blogs'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar/>
-      
+      <Blogs/>
+      <Footer/>
     </>
   )
 }
